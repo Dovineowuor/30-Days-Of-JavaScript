63e63d5ff2ece1ffcabb0123e15ec479b3f59d2b
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-03 22:01:02 +0300

    inttro to variables

---
+++ dist/dist/variables.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// declaring a variable on JS
+var person; // Assigning values to a variable in js
+
+var person = "John";
+console.log("Hello", person);
+console.log(person); // reassignmentof variables to a new value
+
+var person = "Dove";
+console.log("Hi", person);
+console.log(person);
