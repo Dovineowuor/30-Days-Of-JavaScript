920ee9a84454fd46294e2466a3bfe153bdffa1cf
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-04 00:03:57 +0300

    boolean

---
+++ dist/booleans.dev.js
@@ -0,0 +1,29 @@
+"use strict";
+
+console.log(1 > 2);
+/*false*/
+
+console.log(1 == 2);
+/*false*/
+
+var score = 100;
+console.log(score == 101);
+/*false*/
+
+console.log(2 == 2);
+/*true*/
+
+console.log(100 == "100");
+/*true*/
+
+console.log(100 === "100");
+/*false*/
+
+console.log(1 != 2);
+/*true*/
+
+console.log(1 !== 1);
+/*false*/
+
+console.log(1 !== "1");
+/*true*/
