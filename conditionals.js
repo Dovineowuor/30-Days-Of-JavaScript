5f5297669d0cf6c2342e757baa43eeb0506a9a90
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-05 12:29:50 +0300

    Javascript conditionals

---
+++ conditionals.js
@@ -0,0 +1,21 @@
+const grade = 50;
+if (grade>40){
+    console.log("Congratulations you passed!");
+	    }
+else{
+	console.log("Unfortunately you didn't meet the pass minimum thresshold")}
+
+// Switch ststement application. Applied to when there are numerous conditions. Used as an altetrnative of if else.
+//
+//  if, else if, else
+
+var subject = "IT";
+if (subject == "IT") {
+	console.log("Gold Achiever") }
+else if (subject == "SE") {
+	console.log("Silver Buddy" )}
+else if (subject = "ICT") {
+	console.log("Bronze Pal") }
+else {
+	console.log(" You did it, Work harder but smarter next time. Applause")}
+
