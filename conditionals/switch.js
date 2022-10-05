68844fd0fe235e5b240179c62d543cfb730b5b5d
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-05 12:46:54 +0300

    Switch statement

---
+++ conditionals/switch.js
@@ -0,0 +1,15 @@
+var position = "IT";
+switch(position) {
+	case "IT":
+	    console.log("Gold Ranker");
+	    break
+		    case "SE":
+            console.log("Silver Buddy");
+            break
+		    case "ICT":
+	    console.log("Bronze Squencher")
+	    break
+		    default:
+	    console.log(" You did it. You have to work hard but smarter")
+                 }
+
