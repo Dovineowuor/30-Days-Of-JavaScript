cd93443f63dc0a7e6a9ce17ecdbc59a8a36caf64
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-05 13:15:46 +0300

    test2

---
+++ conditionals/test1.js
@@ -0,0 +1,27 @@
+var day = `Sunday`;
+switch(day) {
+   case 'Monday':
+       console.log('Read a book');
+       break;
+   case 'Tuesday':
+       console.log('Watch a movie');
+       break;
+   case 'Wednesday':
+       console.log('Read a book');
+       break;
+   case 'Thursday':
+       console.log('Play basketball');
+       break;
+   case 'Friday':
+       console.log('Socialize');
+       break;
+   case 'Saturday':
+       console.log('Chill');
+       break;
+   case 'Sunday':
+       console.log('Have barbecue');
+       break;
+   default:
+       console.log('There is no such day');
+}
+
