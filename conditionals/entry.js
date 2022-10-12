3e2b2ed874d126ed68241748e55ebc1333e428e2
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-13 00:43:02 +0300

    Entry conditions

---
+++ conditionals/entry.js
@@ -0,0 +1,14 @@
+var amount = 500;
+if (amount <= 100) {
+   console.log('You get your income from your pension')
+} else if (amount < 200 && amount >= 250) {
+   console.log('Each month you get a salary')
+
+} else if (amount < 300 && amount >= 500) {
+   console.log('You get an allowance and a full entry to VIP services!')
+
+} else {
+   console.log('The value of the age variable is not numerical')
+
+}
+
