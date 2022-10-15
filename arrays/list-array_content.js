e9d55faf6535aa1f0e6a0cc36339f6369d9e81b4
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-15 10:04:53 +0300

    list array content func declaration

--- arrays/list-array_content.js
+++ arrays/list-array_content.js
@@ -1,7 +1,14 @@
 #!/bin/node
-var = 
+var school = [
+  'Order Of Merit',
+  'Positions',
+  'Class Teacher',
+  'Stream',
+  'Name',
+  'Course'
+]
 function listArrayItems(arr) {
     for (var i = 0; i < arr.length; i++)
 	console.log(i)
 }
-listArrayItems()
+listArrayItems(school)
