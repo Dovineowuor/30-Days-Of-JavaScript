90c37ee912233348040e0cddfd563008d53a5a5d
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-15 10:36:39 +0300

    A function that finds a slice of a string using its own index: fix bug

--- arrays/letter-finder.js
+++ arrays/letter-finder.js
@@ -1,7 +1,7 @@
 #!/bin/bash
-var test = [" I love JavaScript, It is a relationship where we dont utter any word but each one of us move out satisfied"] 
+const test = [" I love JavaScript, It is a relationship where we dont utter any word but each one of us move out satisfied"] 
 function letterFinder(word, match) {
-    for  i = 0; i < word.length; i++) {
+    for var i = 0; i < word.length; i++) {
 	    if (word[i] == match) {console.log("Yay! We found the", match, "at" i)}
     else {
 	    console.log("---No match found", i)
