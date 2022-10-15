d8937ec5bf7a98d7984e8de40734544b09ecb522
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-15 10:53:03 +0300

    A function that finds a slice of a string using its own index: fix bug ('for'wq unexpected identifier)

--- arrays/letter-finder.js
+++ arrays/letter-finder.js
@@ -1,10 +1,13 @@
-#!/bin/bash
-function letterFinder(word, match) 
-    for (var i = 0; i < word.length; i++) {
-	    if (word[i] == match) {console.log("Yay! We found the", match, "at" i)}
-    else {
-	    console.log("---No match found", i)
-	   }
+function letterFinder(word, match) {
+    for(var i = 0; i < word.length; i++) {
+        if(word[i] == match) {
+            console.log('Found the', match, 'at', i)
+        } else {
+            console.log('---No match found at', i)
+        }
     }
-letterFinder( " I love JavaScript, It is a relationship where we dont utter any word but each one of us move out satisfied"
+}
+
+letterFinder(" I love JavaScript, It is a relationship where we dont utter any word but each one of us move out satisfied"
 , "t")
+
