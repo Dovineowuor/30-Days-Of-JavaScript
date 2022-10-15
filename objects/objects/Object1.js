77af46d3f6434426845742ca696da129a2e82939
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-15 11:21:30 +0300

    Object 1

---
+++ objects/objects/Object1.js
@@ -0,0 +1,11 @@
+#!/bin/node
+var assistantManager = {
+    movement: "Fast"
+    socialSkills: "Good Socialite"
+    streetSmarts: 50
+    health: 60
+    proficiency: "Highly Proficient"
+    role: "Management"
+}
+
+console.log(assistantManager)
