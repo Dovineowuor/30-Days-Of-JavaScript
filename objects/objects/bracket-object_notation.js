f62bc55638c0e8536d41909a399c7ff07d64bb60
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-15 12:07:58 +0300

    Updating objects

---
+++ objects/objects/bracket-object_notation.js
@@ -0,0 +1,10 @@
+var house = {};
+house["rooms"] = 12;
+house['color']= "orange";
+house["priceUSD"] = 10000;
+console.log(house2); // {rooms: 4, color: 'pink', priceUSD: 12345}
+house["windows"] = 15;
+house[state] = "New and reinnovated";
+console.log(house)
+house[manager] = "Eng. Dove"
+console.log(house)
