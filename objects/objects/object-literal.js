4898bcd421dc94ead0a4db4db485da4d2aefad34
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-15 11:38:59 +0300

    Object literal;s

---
+++ objects/objects/object-literal.js
@@ -0,0 +1,8 @@
+var assistantManager = {
+    rangeTilesPerTurn: 3,
+    socialSkills: 30,
+    streetSmarts: 30,
+    health: 40,
+    specialAbility: "young and ambitious",
+    greeting: "Let's make some money"
+}
