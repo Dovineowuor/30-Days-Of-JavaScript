91efe4d2db18881ad669d203393ee9ecc0a9eb03
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-15 10:58:33 +0300

    A function that finds a slice of a string using its own index from a preset variable

---
+++ arrays/letter-finder1.js
@@ -0,0 +1,12 @@
+var test = "I am the one"
+
+function letterFinder(word, match) {
+    for(var i = 0; i < word.length; i++) {
+        if(word[i] == match) {
+            console.log('Found the', match, 'at', i)
+        } else {
+            console.log('---No match found at', i)
+        }
+    }
+}
+letterFinder(test, "a")
