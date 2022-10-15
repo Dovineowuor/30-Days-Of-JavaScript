d98c5280ea844654bad288d6ff5a98b70a2ce81a
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-15 11:46:31 +0300

    Table objects

---
+++ objects/objects/table-object.js
@@ -0,0 +1,11 @@
+var table = {
+    comodity : "Iphone 12 pro-max"
+    model: 3,
+    color: "silver",
+    Specs: "lightweight", 
+    power: 20000
+    manufacturer: "Iphone",
+    priceUSD: 800,
+
+}
+console.log(table)
