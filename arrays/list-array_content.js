0f6d4ce1346913601389d05b3d0ddeb2ab2ad8de
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-15 10:10:07 +0300

    Fix bugs

--- arrays/list-array_content.js
+++ arrays/list-array_content.js
@@ -1,14 +1,7 @@
 #!/bin/node
-var school = [
-  'Order Of Merit',
-  'Positions',
-  'Class Teacher',
-  'Stream',
-  'Name',
-  'Course'
-]
+var school = [ 'Order Of Merit', 'Positions', 'Class Teacher', 'Stream', 'Name', 'Course']
 function listArrayItems(arr) {
     for (var i = 0; i < arr.length; i++)
-	console.log(i)
+	console.log(i, arr[i])
 }
-listArrayItems(school, arr[i])
+listArrayItems(school)
