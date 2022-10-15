03a044e14b2a6740bb9ee15187bcf171c4d287c5
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-15 11:49:47 +0300

    Table objects

--- objects/objects/table-object.js
+++ objects/objects/table-object.js
@@ -1,9 +1,9 @@
 var table = {
-    comodity : "Iphone 12 pro-max"
+    comodity : "Iphone 12 pro-max",
     color: "silver",
     Specs: "lightweight", 
     manufacturer: "Iphone",
-    power: 20000
+    power: 20000,
     priceUSD: 800,
     model: 3,
     
