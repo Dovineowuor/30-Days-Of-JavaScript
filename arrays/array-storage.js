16a140fae73bc3c68480e3ab04d07fac1c292a18
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-15 09:54:40 +0300

    Array trains

---
+++ arrays/array-storage.js
@@ -0,0 +1,10 @@
+#!/bin/node
+var carriage = "Order Of Merit"
+var carriage0 = "Positions"
+var carriage1 = "Class Teacher"
+var carriage2 = "Stream"
+var carriage3 = "Name"
+var carriage4 = "Course"
+
+var train1 = [ carriage, carriage0, carriage1, carriage2, carriage3, carriage4 ]
+console.log(train1)
