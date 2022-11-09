e59357608f2200269e1e0d2c8a6e6932c00e6327
dovineowuor <owuordove@gmail.com>
2022-11-09 09:13:43 +0300

    DOM

---
+++ D-O-M/update-webpage.js
@@ -0,0 +1,8 @@
+#!/bin/node
+// This function updates a website local tree using document object model.
+
+const h2 = document.createElement('h2');
+    h2.innerText = "Welcome To DOM framework";
+    h2.setAttribute = ("id", SubHeading);
+    h2.setAtribute = ("class", Secondary);
+h2
