a977acc94392abb1f1109e504c282a0c2a600eeb
dovineowuor <owuordove@gmail.com>
2022-11-09 09:27:22 +0300

    jquery document querry

---
+++ D-O-M/Jquery.js
@@ -0,0 +1,6 @@
+#!/bin/node
+// This file showcases how javascript queries works
+    document // this returns the whole html document tree associated with this file
+document.querrySelector("p"); // This function querries the first p aragraph element in the html document
+document.getElementById("heading") // This querry asks the server and counterchecks all the posibilities of existence of an id called heading returning all its instances.
+document.getElementByClassName("txt")// This querry aims at collecting all the data in the class named txt returning the findings.
